e33585d76dd6ea1ecf874e94d4efdcf1744aa815
Emina S <emina.ermin24@gmail.com>
2024-03-29 13:48:52 -0400

    Added main.js file

---
+++ main.js
@@ -0,0 +1,10 @@
+const {app, BrowserWindow} = require('electron');
+
+function createWindow(){
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+    window.loadFile('src/index.html');
+}
